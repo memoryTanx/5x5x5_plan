ed566d0f91ea7970829358aa825f0bfdaabb2607
memoryTanx <2946878004@qq.com>
2021-09-14 19:44:41 +0800

    BASE_DIR / 'templates'

--- plan_5x5x5/settings.py
+++ plan_5x5x5/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'plan_5x5x5.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
